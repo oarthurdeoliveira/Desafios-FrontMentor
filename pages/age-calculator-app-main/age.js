ad5623be3891e833565849d32d030f45a617b051
Artnoobbr <artnoobbr@gmail.com>
2024-03-07 14:02:52 -0300

    95% feito

--- pages/age-calculator-app-main/age.js
+++ pages/age-calculator-app-main/age.js
@@ -1,76 +1,91 @@
-var submit = document.getElementById("image_button")
+let submit = document.getElementById("image_button")
 
 submit.addEventListener("click", calcular)
 
 function calcular() {
-    var date = new Date()
-    var dia_atual = date.getDate()
-    var mes_atual = date.getMonth() + 1
-    var ano_atual = date.getFullYear()
+    let date = new Date()
+    let dia_atual = date.getDate()
+    let mes_atual = date.getMonth() + 1
+    let ano_atual = date.getFullYear()
 
     //Input usuario
-    var dia = document.getElementById("dia")
-    var mes = document.getElementById("mes")
-    var ano = document.getElementById("ano")
+    let dia_input = document.getElementById("dia").value
+    let mes_input = document.getElementById("mes").value
+    let ano_input = document.getElementById("ano").value
 
-    var error_log = document.getElementById("error")
+    let res_dia = document.getElementById("res_dia")
+    let res_mes = document.getElementById("res_mes")
+    let res_ano = document.getElementById("res_ano")
 
+    let error_log = document.getElementById("error")
+    error_log.innerHTML = ""
+    let error_test = false
+    
 
-    //Transformar em numero
-    dia = Number(dia.value)
-    mes = Number(mes.value)
-    ano = Number(ano.value)
+    let meses = [31,28,31,30,31,30,31,31,30,31,30,31]
 
-    //Input resultado
-    var res_dia = document.getElementById("res_dia")
-    var res_mes = document.getElementById("res_mes")
-    var res_ano = document.getElementById("res_ano")
+    console.log(`Data atual: ${dia_atual}, ${mes_atual}, ${ano_atual}`)
+    console.log(`Dia input: ${dia_input}, ${mes_input}, ${ano_input}`)
 
-    var meses = [31,28,31,30,31,30,31,31,30,31,30,31]
-
-    var dias_meses_atual = Number(mes_atual - 1)
-
-    var dias_meses = Number(mes - 1)
-
-    if(ano % 4 == 0 && ano % 100 != 0) {
-        meses[1] += 1
-    } else if (ano_atual % 4 == 0 && ano_atual % 100 != 0) {
-        meses[1] += 1
-    }
-
-    if (dia > meses[dias_meses] || dia < 1) {
-        error_log.innerHTML = "Erro de dias!"
-        return
-    } else if (mes > 12 || mes < 1) {
-        error_log.innerHTML = "Erro de meses!"
-        return
-    } else if (ano > ano_atual || ano < 1) {
-        error_log.innerHTML = "Erro de anos!"
-        return
+    if (( ano_input % 4 == 0 && ano_input % 100 != 0 ) || (ano_input % 400 == 0) ) { 
+        console.log("Ano input é bissexto!");
+        meses[1]++ 
     } else {
-        error_log.innerHTML = ""
-    }
-
-    var ano2 = ano_atual - ano
-    var mes2 = mes_atual -  mes
-    var dia2 =  dia_atual - dia
-
-
-    if (dia > dia_atual) {
-        alert("dia é maior que o atual")
-        alert(meses[dias_meses])
-        dia2 += meses[dias_meses_atual]
-        mes2 -= 1
+        console.log("Ano input não é bissexto!");
 
     }
 
-    if (mes >= mes_atual) {
-        mes2 += 12
-        ano2 -= 1
+    if (dia_input < 1 || dia_input > meses[mes_input - 1]) {
+        error_test = true
+    } else if (meses[mes_input - 1] == undefined) {
+        console.log("Mês invalido")
+        error_test = true
+    }
+
+    if (mes_input < 1 || mes_input > 12) {
+        error_test = true
+    }
+
+    if (ano_atual < ano_input) {
+        error_test = true
+    }
+
+    if (error_test == true) {
+        return
     }
 
 
-    res_ano.innerHTML = ano2
-    res_mes.innerHTML = mes2
-    res_dia.innerHTML = dia2
+    if (( ano_atual % 4 == 0 && ano_atual % 100 != 0 ) || (ano_atual % 400 == 0) ) { 
+        console.log("Ano atual é bissexto!");
+        meses[1]++ 
+    } else {
+        console.log("Ano atual não é bissexto!");
+    }
+
+    let dia_calc = dia_atual - dia_input
+    let mes_calc = mes_atual - mes_input
+    let ano_calc = ano_atual - ano_input
+
+
+
+    
+    console.log(meses[1])
+
+    if (dia_atual < dia_input) {
+        console.log("Dia atual é menor que o input")
+        mes_calc--
+        dia_calc += meses[mes_input - 1]
+        console.log(meses[mes_input - 1])
+    }
+
+    if (mes_atual < mes_input) {
+        console.log("Mês atual é menor que o inpuit")
+        ano_calc--
+        mes_calc+= 12
+    }
+
+    res_dia.innerHTML = dia_calc
+    res_mes.innerHTML = mes_calc
+    res_ano.innerHTML = ano_calc
+    
 }
